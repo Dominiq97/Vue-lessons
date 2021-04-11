f506befce302483809b123d1c0b1d9dcc2855e6e
Dominic Catana <catanadominic@gmail.com>
2021-04-11 09:30:25 +0300

    ionic exercises

--- ionic-vue-lessons/src/pages/Conditionals_and_lists.vue
+++ ionic-vue-lessons/src/pages/Conditionals_and_lists.vue
@@ -1,33 +1,79 @@
+<style>
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #1b995e;
+  color: white;
+  text-align: center;
+}
+</style>
+
 <template>
-    <ion-header>
-        <ion-toolbar>
-            <ion-title>Hello</ion-title>
-        </ion-toolbar>
-    </ion-header>
-    <ion-content>
-        <ion-card>
-        <img src="../../public/assets/icon/icon.png" />
-        <ion-card-header>
-          <ion-card-subtitle>Destination</ion-card-subtitle>
-          <ion-card-title>Madison, WI</ion-card-title>
-        </ion-card-header>
-        <ion-card-content>
-          Founded in 1829 on an isthmus between Lake Monona and Lake Mendota, Madison was named the capital of the Wisconsin Territory in 1836.
-        </ion-card-content>
-      </ion-card>
-    </ion-content>
+  <ion-header>
+    <ion-toolbar>
+      <ion-title>Hello</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content>
+    <ion-card>
+      <section id="user-goals">
+        <ion-label>My course goals</ion-label>
+        <ion-input v-model="enteredGoalValue" type="text"></ion-input>
+        <ion-button @click="addGoal">Add Goal</ion-button>
+        <ion-label v-if="goals.length === 0"
+          >No goals have been added yet - please start adding some!</ion-label
+        >
+        <ion-list v-else>
+          <ion-item v-for="(goal,index) in goals" v-bind:key="goal">{{goal}}-{{index}}<ion-button v-on:click="removeGoal(index)">X</ion-button>
+          <ion-input @click.stop></ion-input>
+          </ion-item>
+          
+        </ion-list>
+        <!-- <ion-list>
+          <ion-item v-for="(value,key) in {name:'Max', age:31}" v-bind:key="value">{{key}} - {{value}}</ion-item>
+        </ion-list> -->
+        <!-- <ion-list>
+            <ion-item v-for="num in 10" v-bind:key="num">{{num}}</ion-item>
+        </ion-list> -->
+      </section>
+    </ion-card>
+  </ion-content>
 </template>
 
 <script>
-import { IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/vue';
+import { IonInput, IonItem, IonList, IonLabel } from "@ionic/vue";
 export default {
-    components:{
-        IonHeader, 
-        IonTitle,
-        IonContent,IonToolbar,
+  components: {
+    IonInput,
+    IonItem,
+    IonList,
+    IonLabel,
+  },
+  setup() {},
+  data() {
+    return {
+      goals: [],
+      enteredGoalValue:'',
+
+    };
+  },
+  methods:{
+    addGoal(){
+        this.goals.push(this.enteredGoalValue);
     },
-    setup() {
-        
-    },
-}
+    removeGoal(index){
+        this.goals.splice(index,1);
+    }
+  }
+};
 </script>
